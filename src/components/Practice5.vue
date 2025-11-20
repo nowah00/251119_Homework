dbd814efdd261fa13d059c2e6890902aaca78f79
nowah00 <hawon2k@gmail.com>
2025-11-20 09:29:13 +0900

    Pracrice5 Update

--- src/components/Practice5.vue
+++ src/components/Practice5.vue
@@ -7,11 +7,12 @@ const onClick = () => {
   // 클릭시 inputRef로 포커스
   inputRef.value.focus();
 
+  // inputRef의 value를 Alert로 출력
   alert(inputRef.value.value);
 };
 </script>
 
 <template>
-  <input ref="inputRef" type="text" />
+  <input ref="inputRef" type="text" @keyup.enter="onClick" />
   <button @click="onClick">click</button>
 </template>
